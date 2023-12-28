d260486b39566ccce5fc1c92d90ab115febd99dc
robthomas1120 <robrob0430@gmail.com>
2023-12-29 00:42:10 +0800

    sad page design done

--- app/src/main/java/com/example/formary/sadActivity.kt
+++ app/src/main/java/com/example/formary/sadActivity.kt
@@ -1,11 +1,11 @@
 package com.example.formary
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class sadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,50 +14,51 @@ class sadActivity : AppCompatActivity() {
 
         val homeButton = findViewById<Button>(R.id.homeButtonSad)
 
-        homeButton.setOnClickListener{
-
+        homeButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-
         }
 
-        // Creating an instance of the LonelyDatabaseHandler
+        // Creating an instance of the SadDatabaseHandler
         val sadDbHandler = SadDatabaseHandler(this)
 
-        // Adding sample lonely entries
+        // Adding sample sad entries
         sadDbHandler.addSad("Sad 1", "ano kaya feeling maging sad")
         sadDbHandler.addSad("Sad 2", "sanaol hindi sad")
         sadDbHandler.addSad("Sad 3", "Sadness ang favorite emotion ko sa inside out")
         sadDbHandler.addSad("Sad 4", "huhu ako ay malongcoat")
         sadDbHandler.addSad("Sad 5", "ang longcoat longcoat")
 
-        // Retrieving a random lonely entry
+        // Retrieving a random sad entry
         val randomSad = sadDbHandler.getRandomSad()
         randomSad?.let {
-            Log.d("Random Lonely", "ID: ${it.id}, Title: ${it.title}, Lonely: ${it.sad}")
+            Log.d("Random Sad", "ID: ${it.id}, Title: ${it.title}, Sad: ${it.sad}")
         }
 
-        val sad = randomSad
-        val sadString = sad.toString()
-        val displaySad = findViewById<TextView>(R.id.sadID)
-        displaySad.text = sadString
+        val sadTitle = findViewById<TextView>(R.id.sadTitle)
+        val sadID = findViewById<TextView>(R.id.sadID)
+
+        randomSad?.let {
+            sadTitle.text = it.title
+            sadID.text = it.sad
+        }
 
         val generateSadButton = findViewById<Button>(R.id.randomSadButton)
 
         generateSadButton.setOnClickListener {
 
-            // Calling getRandomLonely on the instance of LonelyDatabaseHandler
+            // Calling getRandomSad on the instance of SadDatabaseHandler
             val randomSad = sadDbHandler.getRandomSad()
 
-            // Check if a random lonely is retrieved and log its details
+            // Check if a random sad is retrieved and log its details
             randomSad?.let {
-                Log.d("Random Lonely", "ID: ${it.id}, Title: ${it.title}, Lonely: ${it.sad}")
+                Log.d("Random Sad", "ID: ${it.id}, Title: ${it.title}, Sad: ${it.sad}")
             }
 
-            val randomSadGetString = randomSad
-            val usableRandomSad = randomSadGetString.toString()
-            displaySad.text = usableRandomSad
+            randomSad?.let {
+                sadTitle.text = it.title
+                sadID.text = it.sad
+            }
         }
-
     }
 }
